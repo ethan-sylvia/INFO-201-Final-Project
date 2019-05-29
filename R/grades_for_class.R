00db890d33ab05a62f79a4acd25d175cdfe64233
ethan-sylvia <49211336+ethan-sylvia@users.noreply.github.com>
2019-05-28 18:03:21 -0700

    Update grades_for_class.R

--- R/grades_for_class.R
+++ R/grades_for_class.R
@@ -1,11 +1,15 @@
 grades <- read.csv("UW-Seattle_20110-20161-Course-Grade-Data_2016-04-06.csv")
 my_function <- function (course_id) {
-  class_grades <- grades[(grep(course_id, grades$Course_Number))]
-  if (dim(class_grades) == NULL) {
+  class_grades <- grades[grep(course_id, grades$Course_Number),]
+  print(dim(class_grades))
+  if (length(class_grades) == 0) {
     return("Course not found")
   } else {
-    return(paste0("The mean grade is ", mean(class_grades$Average_GPA), " The median grade is ")
-           , median(class_grades$Average_GPA), " the minimum grade is ", min(class_grades$Average_GPA),
-           " and the highest grade is ", max(class_grades$Average_GPA))
+    return_phrase <- paste0("The mean grade is ",mean(class_grades$Average_GPA)," The median grade is "
+                            ,median(class_grades$Average_GPA)," the minimum grade is ",
+                            min(class_grades$Average_GPA)," and the highest grade is ",max(class_grades$Average_GPA))
+    return(return_phrase)
   }
 }
+
+  
